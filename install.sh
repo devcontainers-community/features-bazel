3e7fae6749fe4f90c18cca927ef247e3e0856e7c
Jacob Hummer <jcbhmr@outlook.com>
2023-08-17 14:01:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-set -ex
+set -eu
 
-cat <<EOF
-----------------------------------------------
+echo "Installing Bazel"
 
-               YOU ARE AWESOME!
+LOCAL_BIN="/usr/local/bin"
 
-----------------------------------------------
-EOF
+github_download() {
+    repo=$1
+    version=$2
+    artifact=$3
+    output=$4
+    if [ "${version}" = "latest" ]; then
+        URL="https://github.com/${repo}/releases/latest/download/${artifact}"
+    else
+        URL="https://github.com/${repo}/releases/download/${version}/${artifact}"
+    fi
+    curl -LSs "${URL}" -o "${output}"
+}
+
+github_download "bazelbuild/bazelisk" "${BAZELISK_VERSION}" "bazelisk-linux-amd64" "${LOCAL_BIN}/bazelisk"
+chmod +x "${LOCAL_BIN}/bazelisk"
+ln -s "${LOCAL_BIN}/bazelisk" "${LOCAL_BIN}/bazel"
+
+github_download "bazelbuild/buildtools" "${BUILDIFIER_VERSION}" "buildifier-linux-amd64" "${LOCAL_BIN}/buildifier"
+chmod +x "${LOCAL_BIN}/buildifier"
