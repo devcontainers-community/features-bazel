6577c61250348d6560304787dd6fe9592b4fb3ff
Jacob Hummer <jcbhmr@outlook.com>
2023-08-22 16:43:28 -0500

    copy code from https://github.com/Balazs23/devcontainers-features/tree/eac2f3cc44c358e0c2f11b864143eafb4f8c4b37/src/bazel

--- install.sh
+++ install.sh
@@ -1,29 +1,73 @@
 #!/bin/bash
-set -eu
-source lib.sh
+set -e
 
-check_packages curl ca-certificates
+# bazelisk version for Dev Container CLI
+BAZELISK_VERSION=${BAZELISK:-"latest"}
+BAZELISK_GIT_REPO="bazelbuild/bazelisk"
+BAZELISK_LOCAL_PATH="/usr/local/bin/bazelisk"
 
-echo "Installing Bazel"
+# Check user
+if [ "$(id -u)" -ne 0 ]; then
+	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+	exit 1
+fi
 
-LOCAL_BIN="/usr/local/bin"
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
-github_download() {
-    repo=$1
-    version=$2
-    artifact=$3
-    output=$4
-    if [ "${version}" = "latest" ]; then
-        URL="https://github.com/${repo}/releases/latest/download/${artifact}"
-    else
-        URL="https://github.com/${repo}/releases/download/${version}/${artifact}"
+apt_get_update_if_needed()
+{
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        apt-get update
     fi
-    curl -LSs "${URL}" -o "${output}"
 }
 
-github_download "bazelbuild/bazelisk" "${BAZELISK_VERSION}" "bazelisk-linux-amd64" "${LOCAL_BIN}/bazelisk"
-chmod +x "${LOCAL_BIN}/bazelisk"
-ln -s "${LOCAL_BIN}/bazelisk" "${LOCAL_BIN}/bazel"
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update_if_needed
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
 
-github_download "bazelbuild/buildtools" "${BUILDIFIER_VERSION}" "buildifier-linux-amd64" "${LOCAL_BIN}/buildifier"
-chmod +x "${LOCAL_BIN}/buildifier"
+install_bazelisk() {
+    check_packages curl ca-certificates grep sed
+
+    arch="$(uname -m)"
+    case "${arch}" in
+        x86_64) ARCHITECTURE=amd64 ;;
+        aarch64 | arm64 | armv8*) ARCHITECTURE=arm64 ;;
+        *)
+            echo -e "Bazelisk does not support current '$arch' architecture, check releases https://github.com/${BAZELISK_GIT_REPO}/releases"
+            exit 1
+    esac
+
+    platf="$(uname -s)"
+    case "${platf}" in
+        Linux) PLATFORM=linux ;;
+        Darwin) PLATFORM=darwin ;;
+        *)
+            echo -e "Unsupported '$platf' platform"
+            exit 1
+    esac
+
+    if [ "${BAZELISK_VERSION}" = "latest" ]; then
+        # curl "https://api.github.com/repos/${BAZELISK_GIT_REPO}/releases/latest"
+        BAZELISK_VERSION=$(curl --silent "https://api.github.com/repos/${BAZELISK_GIT_REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    fi
+
+    echo "(*) Installing Bazelisk $PLATFORM $ARCHITECTURE $BAZELISK_VERSION"
+
+    curl -fSsL -o $BAZELISK_LOCAL_PATH "https://github.com/${BAZELISK_GIT_REPO}/releases/download/${BAZELISK_VERSION}/bazelisk-${PLATFORM}-${ARCHITECTURE}"
+    chmod 0755 $BAZELISK_LOCAL_PATH
+
+    # Adds symlink as bazel to bazelisk
+    ln -s /usr/local/bin/bazelisk /usr/local/bin/bazel
+}
+
+install_bazelisk
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done"
